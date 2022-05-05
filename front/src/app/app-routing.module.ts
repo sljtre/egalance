46ba56eba6ff576477338ff7670cf3b974e92fc9
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-05-05 09:16:13 +0200

    thème sombre + structure

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -10,7 +10,35 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'game',
+    loadChildren: () => import('./game/game.module').then( m => m.GamePageModule)
   },
+  {
+    path: 'friends',
+    loadChildren: () => import('./friends/friends.module').then( m => m.FriendsPageModule)
+  },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'scoreboard',
+    loadChildren: () => import('./scoreboard/scoreboard.module').then( m => m.ScoreboardPageModule)
+  },
+  {
+    path: 'rules',
+    loadChildren: () => import('./rules/rules.module').then( m => m.RulesPageModule)
+  },
+  {
+    path: 'reset-password',
+    loadChildren: () => import('./reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
+  },
+  {
+    path: 'conf-account',
+    loadChildren: () => import('./conf-account/conf-account.module').then( m => m.ConfAccountPageModule)
+  },
+
 ];
 
 @NgModule({
---
+++ front/src/app/conf-account/conf-account.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conf-account',
+  templateUrl: './conf-account.page.html',
+  styleUrls: ['./conf-account.page.scss'],
+})
+export class ConfAccountPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
