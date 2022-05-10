978a931ef7c5076642a8eb1285507292b81b24bc
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-05-10 14:44:30 +0200

    clock

--- front/src/app/game/game.page.ts
+++ front/src/app/game/game.page.ts
@@ -8,15 +8,50 @@ import { Component, OnInit } from '@angular/core';
 export class GamePage implements OnInit {
 
   public matrix = [];
+  public clock;
+  public paused = false;
+
+  public day = 1;
+  public month = 1;
+  public year = 2023;
+
+  public delay = 1000;
 
   constructor() { }
 
   ngOnInit() {
-    console.log('bouh');
     for(let i=0;i<50;i++) {
       this.matrix.push({type: 'usine'});
     }
-    console.log('matrix : ', this.matrix);
+    this.clock = setInterval(this.calendar, this.delay);
   }
+
+  calendar = () => {
+    this.addTime(1);
+  };
+
+  pause = () => {
+    if(this.paused){
+      this.paused = false;
+      this.clock = setInterval(this.calendar, this.delay);
+    }else{
+      this.paused = true;
+      clearInterval(this.clock);
+    }
+  };
+
+  addTime = (days) => {
+    this.day += days;
+    if(this.day>30){
+      this.day-=30;
+      this.month++;
+      this.addTime(0);
+    }
+    if(this.month>12){
+      this.month-=12;
+      this.year++;
+      this.addTime(0);
+    }
+  };
 
 }
