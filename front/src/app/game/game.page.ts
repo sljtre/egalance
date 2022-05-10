b2492115627640f7b2e219061bd5f9ecf48c3d10
Simon <72396434+sljtre@users.noreply.github.com>
2022-05-10 11:13:33 +0200

    adjust autumn transition

--- front/src/app/game/game.page.ts
+++ front/src/app/game/game.page.ts
@@ -12,14 +12,18 @@ export class GamePage implements OnInit {
   private currentSeason: number;
   private changeSeason: () => void;
   private launchSummerAnimation: () => void;
+  private launchAutumnAnimation: () => void;
 
   constructor() {
     this.currentSeason = 0;
     this.changeSeason = () => {
-      const seasons = ['winter', 'spring', 'summer', 'autumn'];
+      const seasons = ['spring', 'summer', 'autumn', 'winter'];
       switch (this.currentSeason) {
-        case 2:
+        case 1:
           this.launchSummerAnimation();
+          break;
+        case 2:
+          this.launchAutumnAnimation();
           break;
       }
       const bgElement = document.getElementById('animated-bg');
@@ -34,10 +38,17 @@ export class GamePage implements OnInit {
         sun.className = '';
       }, 5000);
     };
+    this.launchAutumnAnimation = () => {
+      const leaves = document.querySelector('#leaves');
+      leaves.className = 'animated';
+      setTimeout(() => {
+        leaves.className = '';
+      }, 15000);
+    };
   }
 
   ngOnInit() {
-    setInterval(this.changeSeason, 3000);
+    setInterval(this.changeSeason, 5000);
     for (let i = 0; i < 50; i++) {
       this.matrix.push({type: 'usine'});
     }
