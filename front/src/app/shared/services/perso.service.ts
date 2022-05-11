ee4489da8d6201eca50425924a6ff7023934f695
Nathan-Kelo <58146047+Nathan-Kelo@users.noreply.github.com>
2022-05-11 15:13:34 +0200

    NK-deplacement des calcul de fatigue,sante,faim

--- front/src/app/shared/services/perso.service.ts
+++ front/src/app/shared/services/perso.service.ts
@@ -18,4 +18,31 @@ export class PersoService {
     this.perso.customization(religion, gender, skin, name);
   };
 
+  dev = (loc, religion, gender, skin, name) => {
+    this.perso = new Perso(loc);
+    this.perso.customization(religion, gender, skin, name);
+  };
+  
+  faim=()=>{
+   this.perso.calculFaim();
+  }
+
+  fatigue=()=>{
+    this.perso.calculFatigue();
+  }
+
+  sante=()=>{
+    this.perso.calculSante();
+  }
+
+  malade=()=>{
+    this.perso.fallSick();
+  }
+
+  calculAll=()=>{
+    this.perso.calculFaim();
+    this.perso.calculFatigue();
+    this.perso.calculSante();
+  }
+
 }
