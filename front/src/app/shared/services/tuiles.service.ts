4851b71005a0fa30831997f4275faad559228f5f
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-05-16 13:17:46 +0200

    patch banque

--- front/src/app/shared/services/tuiles.service.ts
+++ front/src/app/shared/services/tuiles.service.ts
@@ -415,7 +415,7 @@ export class TuilesService {
     }],
     [{
       name: 'Bank',
-      labels: ['bibliotheque'],
+      labels: ['banque'],
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
       actions: ['Deposit', 'Pull', 'Work']
     }],
