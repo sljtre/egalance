95f4115a18829b3913f0ce742e526490e749e578
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-05-19 20:12:53 +0200

    pour mon Nathan d'amur <3

--- front/src/app/shared/services/game-actions.service.ts
+++ front/src/app/shared/services/game-actions.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {PersoService} from '../services/perso.service'
 import {TuilesService} from '../services/tuiles.service'
-import { time } from 'highcharts';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +11,7 @@ export class GameActionsService {
   private tuileActuelle;
 
   constructor(
-    public persoSerivce: PersoService,
+    public persoService: PersoService,
     public tuilesService:TuilesService
   ) {
 
@@ -26,13 +25,27 @@ export class GameActionsService {
       case 'Eat':
         result=this.eatHandler();
         break;
+      case 'Work':
+        result=this.workHandler();
+        break;
       default:break;
     }
     return result;
-
   }
 
-  workHandler=()=>{  }
+  workHandler=()=>{
+    const coefStudies = this.tuilesService.getIncomeMultiplicator(this.tuileActuelle, this.persoService.perso.instructionLevel);
+    const tolerance = 1-this.tuilesService.getTolerance(this.persoService.perso.localization);
+    let differenceScore = Math.abs(Number(this.tuilesService.getEthnie(this.persoService.perso.localization))
+      -Number(this.persoService.perso.skin));
+    if(this.persoService.perso.religion!==this.tuilesService.getReligion(this.persoService.perso.localization)){
+      differenceScore++;
+    }
+    console.log('difference : ', differenceScore);
+    const coefSalaire = 1-tolerance*0.2*differenceScore;
+    this.persoService.perso.wallet+= this.tuilesService.getSalaire(this.persoService.perso.localization)*coefSalaire;
+    return 30;
+  }
 
   marryHandler=()=>{  }
 
@@ -41,7 +54,7 @@ export class GameActionsService {
   shoppingHandler=()=>{  }
 
   drinkHandler=()=>{
-    this.persoSerivce.perso.faim+=0.1;
+    this.persoService.perso.faim+=0.1;
 
    }
 
@@ -68,37 +81,37 @@ export class GameActionsService {
     if (this.tuileActuelle==='Bar'){
       //cost some time
       //need to add some food smartly
-      this.persoSerivce.perso.faim+=0.1;  
-      this.persoSerivce.perso.fatigue-=0.01;
+      this.persoService.perso.faim+=0.1;
+      this.persoService.perso.fatigue-=0.01;
       //TODO : cout en fonction du salaire moen du pays
-      this.persoSerivce.perso.Wallet-=Number(this.tuilesService.getSalaire(this.actionCity))/100*0.8; 
+      this.persoService.perso.Wallet-=Number(this.tuilesService.getSalaire(this.actionCity))/100*0.8;
       timeadd=3;
 
     }
     else if (this.tuileActuelle==='Farm'){
       //need to add some food smartly
-      this.persoSerivce.perso.faim+=0.5;  
-      this.persoSerivce.perso.fatigue-=0.05;
+      this.persoService.perso.faim+=0.5;
+      this.persoService.perso.fatigue-=0.05;
       //TODO : cout en fonction du salaire moen du pays
-      this.persoSerivce.perso.Wallet-=Number(this.tuilesService.getSalaire(this.actionCity))/100*0.2; 
+      this.persoService.perso.Wallet-=Number(this.tuilesService.getSalaire(this.actionCity))/100*0.2;
       timeadd=8;
 
     }
     else if (this.tuileActuelle==='House'){
       //need to add some food smartly
-      this.persoSerivce.perso.faim+=0.14;  
-      this.persoSerivce.perso.fatigue-=0.014;
+      this.persoService.perso.faim+=0.14;
+      this.persoService.perso.fatigue-=0.014;
       //TODO : cout en fonction du salaire moen du pays
-      this.persoSerivce.perso.Wallet-=Number(this.tuilesService.getSalaire(this.actionCity))/100*0.5; 
+      this.persoService.perso.Wallet-=Number(this.tuilesService.getSalaire(this.actionCity))/100*0.5;
       timeadd=5;
 
     }
     else if (this.tuileActuelle==='Restaurant'){
       //need to add some food smartly
-      this.persoSerivce.perso.faim+=0.3;  
-      this.persoSerivce.perso.fatigue-=0.03;
+      this.persoService.perso.faim+=0.3;
+      this.persoService.perso.fatigue-=0.03;
       //TODO : cout en fonction du salaire moen du pays
-      this.persoSerivce.perso.Wallet-=Number(this.tuilesService.getSalaire(this.actionCity))/100*1.6; 
+      this.persoService.perso.Wallet-=Number(this.tuilesService.getSalaire(this.actionCity))/100*1.6;
       timeadd=3;
     }
     return timeadd;
